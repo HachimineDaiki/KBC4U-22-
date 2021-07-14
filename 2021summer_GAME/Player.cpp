8366b1b9a82df2000be6e7d6c3df8798543d989c
HachimineDaiki <daiki-hachimine-19h@kbc.ac.jp>
2021-07-14 10:24:21 +0900

    前に進む（あとは角度だけ）

--- 2021summer_GAME/Player.cpp
+++ 2021summer_GAME/Player.cpp
@@ -6,14 +6,19 @@ void P_move() {
     switch (Input_PlayerMoveDir())
     {
     case Left:sph[0].z += sph[0].speed;
-         Left:sph[0].x -= sph[0].speed/1.5;
+         Left:sph[0].x -= sph[0].speed * VECTOR_SLIDE;
         break;
-    case Right:sph[0].x += sph[0].speed;
+    case Right:sph[0].x += sph[0].speed * VECTOR_SLIDE;
+        sph[0].z += sph[0].speed;
         break;
-    case Down:sph[0].z -= sph[0].speed;
-        break;
+   /* case Down:sph[0].z -= sph[0].speed;
+        break;*/
     case Up:sph[0].z += sph[0].speed;
-        break;
+        break; 
+    }
+
+    if (sph[0].z < 10000) {
+        sph[0].z += sph[0].speed;
     }
 
     switch (Input_PlayerMoveDir())
@@ -24,8 +29,8 @@ void P_move() {
         break;
     case Down:DrawFormatString(100, 300, GetColor(255, 255, 255), "[‰º]");
         break;
-    case Up:DrawFormatString(100, 300, GetColor(255, 255, 255), "[ã]");
-        break;
+    /*case Up:DrawFormatString(100, 300, GetColor(255, 255, 255), "[ã]");
+        break;*/
     }
 }
 
@@ -48,6 +53,9 @@ int Input_PlayerMoveDir() {
     }
     if (CheckHitKey(KEY_INPUT_W))
     {
+        input_dir = Up;
+    }
+    if (CheckHitKey(KEY_INPUT_SPACE)) {
         input_dir = Up;
     }
 
