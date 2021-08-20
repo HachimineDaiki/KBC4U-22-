c661a2d3dccfbed57f06860a396631b19a971c46
ZAHA61 <65996616+ZAHA61@users.noreply.github.com>
2021-08-20 10:48:03 +0900

    透明の壁追加しました

--- 2021summer_GAME/3Dmodel.cpp
+++ 2021summer_GAME/3Dmodel.cpp
@@ -6,14 +6,12 @@ float wood_interval = 1300;//木の間隔
 void Model3d_load() {
 	//Stage15
 	//Stage_Short.mv1
-	ground.handle = MV1LoadModel("3Dmodel/Stage_Short_1.1");
+	//Stage_Short_1.1
+	ground.handle = MV1LoadModel("3Dmodel/Stage_Short_2.0");
 	rock[0].handle = MV1LoadModel("3Dmodel/Rock.mv1");
 	rock[1].handle = MV1LoadModel("3Dmodel/Rock_1.mv1");
 	rock[2].handle = MV1LoadModel("3Dmodel/Rock_red.mv1");
-	//壁のモデル読み込み
-	//for (int i = 0; i < WALL_MAX; i++) {
-	//	wall[i].handle = MV1LoadModel("3Dmodel/Wall_1.mv1");
-	//}
+
 	//ぬかるみ床
 	for (int i = 0; i < DECELE_ARIA_MAX; i++) {
 		gensoku[i].handle = MV1LoadModel("3Dmodel/MuDColor1.mv1");
@@ -54,19 +52,7 @@ void Model3d_init() {
 	MV1SetRotationXYZ(wall[0].handle, VGet(10.0f * DX_PI_F / 180.0f, 0.0f, 0.0f));
 	MV1SetRotationXYZ(wall[1].handle, VGet(10.0f * DX_PI_F / 180.0f, 0.0f, 0.0f));
 	MV1SetRotationXYZ(ground.handle, VGet(5.0f * DX_PI_F / 180.0f, 0.0f, 0.0f));
-	//for (int i = 0; i < TREE_NUM / 4; i++) {
-	//	MV1SetPosition(tree_handle[i], VGet(-1200.0f, ground.y, -650.0f + (wood_interval * i)));
-	//	//MV1SetRotationXYZ(tree_handle[i], VGet(10.0f, 0.0f, 0.0f));
 
-	//	/*MV1SetPosition(tree_handle[i + TREE_NUM / 4], VGet(-550.0f, ground.y, 0.0f + (wood_interval * i)));
-	//	MV1SetRotationXYZ(tree_handle[i + TREE_NUM / 4], VGet(45.0f * DX_PI_F / 180.0f, 0.0f, 0.0f));
-
-	//	MV1SetPosition(tree_handle[i + TREE_NUM / 2], VGet(-550.0f + wood_interval, ground.y, 0.0f + (wood_interval * i)));
-	//	MV1SetRotationXYZ(tree_handle[i + TREE_NUM / 2], VGet(45.0f * DX_PI_F / 180.0f, 0.0f, 0.0f));
-
-	//	MV1SetPosition(tree_handle[i + TREE_NUM / 4 + TREE_NUM / 2], VGet(100.0f + wood_interval, ground.y, -650.0f + (wood_interval * i)));
-	//	MV1SetRotationXYZ(tree_handle[i + TREE_NUM / 4 + TREE_NUM / 2], VGet(45.0f * DX_PI_F / 180.0f, 0.0f, 0.0f));*/
-	//}
 
 	MV1SetupCollInfo(ground.handle, -1);
 }
@@ -76,24 +62,16 @@ void Model3d_draw() {
 	MV1DrawModel(ground.handle);
 	MV1DrawModel(rock[rock[0].handle_num].handle);
 	
-	//壁モデル描画
-	//for (int i = 0; i < WALL_MAX; i++) {
-	//	MV1DrawModel(wall[i].handle);
-	//}
 	for (int i = 0; i < DECELE_ARIA_MAX; i++) {
 		MV1DrawModel(gensoku[i].handle);
 		MV1SetRotationXYZ(gensoku[i].handle, VGet(10.0f * DX_PI_F / 180.0f, 0.0f, 0.0f));
 	}
-	//SetUseLighting(FALSE);//ライティング計算オフ
 }
 
 void Model3d_dlet() {
 	//// モデルハンドルの削除
 	MV1DeleteModel(ground.handle);
-	//壁モデル削除
-	//for (int i = 0; i < WALL_MAX; i++) {
-	//	MV1DeleteModel(wall[i].handle);
-	//}
+
 	for (int i = 0; i < PLYAER_COLOR; i++) { //プレイヤー色
 		MV1DeleteModel(rock[i].handle);
 	}
